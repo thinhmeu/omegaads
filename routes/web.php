09db25f36af7d95c455285e6019e6d10e80202c2
thinh <thinhmeu.tntn@gmail.com>
2025-03-09 15:26:08 +0700

    test view kml

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::any('/404.html', 'RouterController@not_found');
 
 Route::get("/", "HomeController@index");
 Route::get('ngo-20-ho-tung-mau.kml', function (){
+    define('IS_MOBILE', false);
     return view('ngo20');
 });
 Route::get('ngo-26-ho-tung-mau.kml', function (){
+    define('IS_MOBILE', false);
     return view('ngo26');
 });
